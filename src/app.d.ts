abd8eacf619be9544f1eb4d622f46a8b39999173
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2024-01-22 02:12:36 +0600

    added simple login and signup
    added superform package

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,9 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		// interface PageState {}
+		interface PageState {
+			modalData: any;
+		}
 		// interface Platform {}
 	}
 }
--- src/routes/section_card.svelte
+++ src/routes/section_card.svelte
@@ -2,9 +2,10 @@
     export let icon_path: string;
     export let title: string;
     export let icon_alt = "";
+    export let url="/";
 </script>
 
-<div class="flex aspect-square w-3/4 sm:w-1/2 lg:w-1/4 flex-col justify-center border-[3px] border-primary p-10 align-middle">
+<a href="{url}" class="flex aspect-square w-3/4 sm:w-1/2 lg:w-1/4 flex-col justify-center border-[3px] border-primary p-10 align-middle">
     <div class="items-middle m-auto flex aspect-square w-1/2 justify-center">
         <img
             class="m-auto w-full"
@@ -16,4 +17,4 @@
     <h2 class="mt-3 scroll-m-20 text-center text-2xl font-extrabold">
         {title}
     </h2>
-</div>
+</a>
