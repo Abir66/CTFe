dca013ef799aac613433cc48648816d5a1f98d14
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2024-01-25 11:19:14 +0600

    Added icons in homepage

--- src/routes/section_card.svelte
+++ src/routes/section_card.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
-    export let icon_path: string;
-    export let title: string;
-    export let icon_alt = "";
-    export let url="/";
+	import * as Card from '$lib/components/ui/card';
+	export let icon_path: string;
+	export let title: string;
+	export let icon_alt = '';
+	export let url = '/';
 </script>
 
-<a href="{url}" class="flex aspect-square w-3/4 sm:w-1/2 lg:w-1/4 flex-col justify-center border-[3px] border-primary p-10 align-middle">
-    <div class="items-middle m-auto flex aspect-square w-1/2 justify-center">
-        <img
-            class="m-auto w-full"
-            src="{icon_path}"
-            alt="{icon_alt}"
-        />
-    </div>
+<Card.Root
+	class="hover:shadow-slate-500 hover:shadow-lg duration-250 aspect-square w-3/4 transition hover:scale-110 lg:hover:scale-125 hover:border-primary sm:w-1/2 lg:w-1/4"
+>
+	<a href={url} class="flex h-full w-full flex-col justify-center p-10 align-middle">
+		<div class="items-middle m-auto flex aspect-square w-1/2 justify-center">
+			<img class="dark:stroke-white dark:stroke-2 m-auto w-full" src={icon_path} alt={icon_alt} />
+		</div>
 
-    <h2 class="mt-3 scroll-m-20 text-center text-2xl font-extrabold">
-        {title}
-    </h2>
-</a>
+		<h2 class="mt-3 scroll-m-20 text-center text-2xl font-extrabold">
+			{title}
+		</h2>
+	</a>
+</Card.Root>
