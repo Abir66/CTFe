54c472ead7b21e9f06c10df74b4885796d1ed923
lara-62 <1905062@ugrad.cse.buet.ac.bd>
2024-01-28 01:07:32 +0600

    remove some loged comments and make all schemas name as plural

--- src/routes/contests/+page.server.ts
+++ src/routes/contests/+page.server.ts
@@ -2,8 +2,8 @@
 
 export const load = async ({url,fetch,params,locals: { supabase }}) => {
     let { data, error } = await supabase.rpc('get_contests');
-    console.log(supabase);
-   console.log(data[0].start_time);
+//     console.log(supabase);
+//    console.log(data[0].start_time);
 
     if (error) {
         console.error(error);
