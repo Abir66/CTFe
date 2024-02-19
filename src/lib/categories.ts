71ed2987a870709e66e0d771716a3aa46978dc86
Abir Muhtasim <1905066@ugrad.cse.buet.ac.bd>
2024-02-19 06:18:02 +0600

    submission list done without pagination

---
+++ src/lib/categories.ts
@@ -0,0 +1,19 @@
+const categories = [
+    'Cryptography',
+    'Web',
+    'Pwn',
+    'Reverse',
+    'Forensic',
+    'Misc',
+    'Steganography',
+    'OSINT',
+    'Hardware',
+    'Mobile',
+    'Networking',
+    'Scripting',
+    'Programming',
+    'Binary',
+]
+
+export default categories
+
